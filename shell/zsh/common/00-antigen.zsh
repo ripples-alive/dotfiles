bfe501e7c2df4911561dd4ae2fa8ac89f167ca18
ripples <564311873@qq.com>
2020-05-02 01:09:57 +0800

    feat(zsh): migrate to p10k

--- shell/zsh/common/00-antigen.zsh
+++ shell/zsh/common/00-antigen.zsh
@@ -24,4 +24,7 @@ antigen bundles <<EOF
 EOF
 
 # load the theme
-antigen theme $ZSH_CUSTOM/theme ripples-ys
+antigen theme romkatv/powerlevel10k
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
